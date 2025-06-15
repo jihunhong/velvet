c96e66c47d060809f63e7d9dae3c5f1ab9d93397
jihunhong <redgee49@gmail.com>
2025-06-15 22:01:10 +0900

    fix: chart height 수정

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -54,7 +54,7 @@ export default function ExpenseList() {
         </div>
 
         <div className="col-span-3 h-32 w-full">
-          <ResponsiveContainer width="100%" height="100%">
+          <ResponsiveContainer width="100%" height="118%">
             <AreaChart data={weeklyExpenses} margin={{ top: 10, right: 10, left: 0, bottom: 10 }}>
               <defs>
                 <linearGradient id="colorValue" x1="0" y1="0" x2="0" y2="1">
