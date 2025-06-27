75b5a43991b4e44e55b7114e3f89f37857adc8ed
jihunhong <redgee49@gmail.com>
2025-06-28 01:44:15 +0900

    fix: WeeklyChart 툴팁 수정

--- src/components/WeeklyDotChart/index.tsx
+++ src/components/WeeklyDotChart/index.tsx
@@ -1,10 +1,10 @@
-import { useMemo } from 'react';
+import { CSSProperties, useMemo } from 'react';
 import Dot from './Dot';
 import { calculateDots, formatCount } from './utils';
 
 interface WeeklyDotChartProps {
   weeks: number[]; // 주차별 소비 건수 배열 (최대 5개)
-  maxHeight?: number;
+  maxHeight?: CSSProperties['maxHeight'];
   containerClassName?: string;
   dotClassName?: string;
   dotColor: string;
@@ -24,7 +24,7 @@ const WeeklyDotChart: React.FC<WeeklyDotChartProps> = ({ weeks: data, containerC
   return (
     <div className={`${containerClassName}`}>
       <div className="flex items-end justify-center gap-1 h-20">
-        {weeklyData.map((week) => (
+        {weeklyData.map((week, index) => (
           <div key={week.week} className="flex flex-col items-center group relative">
             <div className="flex flex-col-reverse gap-1 mb-3">
               {Array.from({ length: week.dots }).map((_, dotIndex) => (
@@ -32,9 +32,9 @@ const WeeklyDotChart: React.FC<WeeklyDotChartProps> = ({ weeks: data, containerC
               ))}
             </div>
 
-            <div className="absolute bottom-full mb-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
-              <div className="bg-gray-800 text-white text-xs rounded px-2 py-1 whitespace-nowrap">
-                {formatCount(week.amount)}
+            <div className="absolute bottom-full mb-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none  z-10">
+              <div className="relative bg-gray-800 text-white text-xs rounded px-2 py-1 whitespace-nowrap">
+                {`${index + 1}주차 - ${formatCount(week.amount)}`}
                 <div className="absolute top-full left-1/2 transform -translate-x-1/2 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-800"></div>
               </div>
             </div>
