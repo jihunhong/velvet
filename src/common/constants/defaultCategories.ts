b73a4bb50918d1891046132863d8cfbfb1cc5dd8
jihunhong <redgee49@gmail.com>
2025-06-29 04:01:38 +0900

    chore: init 관련 객체 작성

---
+++ src/common/constants/defaultCategories.ts
@@ -0,0 +1,23 @@
+export const food = '#e7345f';
+export const housing = '#9bd248';
+export const utils = '#FFD93D';
+export const transportation = '#6C5CE7';
+export const health = '#FF8B94';
+export const hobby = '#FF6B35';
+export const shopping = '#FF9F1C';
+export const saving = '#2EC4B6';
+export const unclassified = '#95A5A6';
+export const etc = '#95A5A6';
+
+export const defaultCategories = [
+  { value: 'food', name: '식비', color: food, id: 1 },
+  { value: 'housing', name: '주거비', color: housing, id: 2 },
+  { value: 'utils', name: '공과금,통신비', color: utils, id: 3 },
+  { value: 'transportation', name: '교통비', color: transportation, id: 4 },
+  { value: 'health', name: '의료,건강', color: health, id: 5 },
+  { value: 'hobby', name: '취미,여가', color: hobby, id: 6 },
+  { value: 'shopping', name: '쇼핑', color: shopping, id: 7 },
+  { value: 'saving', name: '저축,투자', color: saving, id: 8 },
+  { value: 'unclassified', name: '미분류', color: unclassified, id: 9 },
+  { value: 'etc', name: '기타', color: etc, id: 10 },
+];
