5dfb2140373a882d8019eb5e655d3ac6b4612fa8
jihunhong <redgee49@gmail.com>
2025-06-30 03:06:17 +0900

    fix: 사용하지 않는 함수 삭제

--- src/db/expenseDB.ts
+++ src/db/expenseDB.ts
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 import { WITH_TIME_ZONE } from '../common/constants/dateFormat';
 import { withTimestamps } from '../common/utils/timestamp';
 
-import { DailyExpenseSummary, Expense, ExpenseFormData, ExpenseRecord } from '../types/expense';
+import { Expense, ExpenseFormData, ExpenseRecord } from '../types/expense';
 import { getDB } from './velvetDB';
 
 export const setExpense = async (expenses: Expense[]) => {
@@ -43,34 +43,8 @@ export const getCurrentMonthExpenses = async () => {
   return await getExpensesByDateRange({ startDate, endDate });
 };
 
-export const getRecentWeekDailySummary = async () => {
-  const endDate = dayjs().format('2025-06-20');
-  const startDate = dayjs().subtract(6, 'day').format(WITH_TIME_ZONE); // 오늘 포함 7일
-  const expenses = await getExpensesByDateRange({ startDate, endDate });
-  // 7일간의 모든 날짜를 먼저 생성 (데이터가 없는 날도 0으로 표시하기 위해)
-  const dateRange: DailyExpenseSummary[] = [];
-  for (let i = 6; i >= 0; i--) {
-    const date = dayjs(endDate).subtract(i, 'day').format(WITH_TIME_ZONE);
-    dateRange.push({
-      date,
-      amount: 0,
-      count: 0,
-    });
-  }
-  expenses.forEach((expense) => {
-    const dayIndex = dateRange.findIndex((day) => day.date === expense.updatedAt);
-    if (dayIndex !== -1) {
-      dateRange[dayIndex].amount += expense.amount;
-      dateRange[dayIndex].count += 1;
-    }
-  });
-  return dateRange;
-};
-
-export const getRecentWeekAmounts = async () => {
-  const dailySummary = await getRecentWeekDailySummary();
-  return dailySummary.map((day) => ({
-    date: day.date,
-    amount: day.amount,
-  }));
+export const getWeeklyExpenses = async () => {
+  const startDate = dayjs().startOf('week').format(WITH_TIME_ZONE);
+  const endDate = dayjs().endOf('week').format(WITH_TIME_ZONE);
+  return await getExpensesByDateRange({ startDate, endDate });
 };
